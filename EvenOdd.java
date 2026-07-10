67578fb8314eff6855effec1a4a592e28a5da866
Rohinee Patil <rohineep27@gmail.com>
2026-07-10 11:17:39 +0530

    Added  EvenOdd program.

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String args[])
+    {
+        int a = 108;
+
+        if (a % 2 == 0) {
+            System.out.println(a + " is Even Number.");
+        } else {
+            System.out.println(a + " is Odd Number.");
+        }
+    }
+}
